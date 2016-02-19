bf15a0708678255f45945dbd9eda24e3a11ab88e
Anand Sakthivel <anandts88@gmail.com>
2016-02-18 23:49:32 -0500

    Blueprints

--- blueprints/ember-validator/index.js
+++ blueprints/ember-validator/index.js
@@ -4,7 +4,7 @@ var RSVP = require('rsvp');
 module.exports = {
   normalizeEntityName: function() {},
 
-  beforeInstall: function() {
+  afterInstall: function() {
     return RSVP.all([
       this.addBowerPackageToProject('moment', '~2.10.6'),
       this.addBowerPackageToProject('lodash', '3.10.1')
